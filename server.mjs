d6368e28241410db066bf3540ddc120bba4f8ec3
Aurelien BOUTEILLER <aurelien.bouteiller@niji.fr>
2023-01-05 17:36:41 +0100

    Add the server code part with package.json

---
+++ server.mjs
@@ -0,0 +1,38 @@
+import { loadSync } from "@grpc/proto-loader";
+import {
+  loadPackageDefinition,
+  Server,
+  ServerCredentials,
+} from "@grpc/grpc-js";
+
+const PROTO_PATH = "./hello_world.proto";
+const packageDefinition = loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+const hello_proto = loadPackageDefinition(packageDefinition);
+
+const sayHello = (call, callback) => {
+  const { name } = call.request;
+  callback(null, { message: `Bonjour ${name}` });
+};
+
+function startServer() {
+  const server = new Server();
+  server.addService(hello_proto.GreeterService.service, { sayHello });
+  const PORT = 3000;
+  const host = `localhost:${PORT}`;
+  server.bindAsync(host, ServerCredentials.createInsecure(), (error) => {
+    if (error) {
+      console.log("An error has occurred in bindAsync", error);
+      return;
+    }
+    server.start();
+    console.log(`Server listening on: ${host}`);
+  });
+}
+
+startServer();
